84a48e0b7c124e77faee8c34ea0011621450a169
neobrinoke2 <alexis.salvador.pro@gmail.com>
2018-03-05 10:30:16 +0100

    php doc

--- application/src/Controller/Controller.php
+++ application/src/Controller/Controller.php
@@ -7,6 +7,14 @@ use GuzzleHttp\Psr7\Response;
 
 class Controller extends Renderer
 {
+	/**
+	 * Send new response
+	 *
+	 * @param int $status
+	 * @param array $headers
+	 * @param null|string $body
+	 * @return Response
+	 */
 	protected function response(int $status = 200, array $headers = [], ?string $body = null)
 	{
 		return new Response($status, $headers, $body);
