4a853a55f7662137c59fe73c912125c028c85594
Neobrinoke <neobrinoke@gmail.com>
2018-03-31 13:30:45 +0200

    file format

--- application/database/migrations/20180331102300_create_game_groups_users_table.php
+++ application/database/migrations/20180331102300_create_game_groups_users_table.php
@@ -7,11 +7,11 @@ class CreateGameGroupsUsersTable extends Migration
 	public function up()
 	{
 		$this->table('game_groups_users', ['signed' => false])
-			->addColumn('user_id', 'integer' , ['signed' => false])
-			->addColumn('game_group_id', 'integer' , ['signed' => false])
+			->addColumn('user_id', 'integer', ['signed' => false])
+			->addColumn('game_group_id', 'integer', ['signed' => false])
 			->addForeignKey('user_id', 'users', 'id', ['delete' => 'cascade'])
 			->addForeignKey('game_group_id', 'game_groups', 'id', ['delete' => 'cascade'])
-		    ->create();
+			->create();
 	}
 
 	public function down()
