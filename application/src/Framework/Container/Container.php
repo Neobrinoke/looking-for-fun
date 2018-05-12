945483fca88e1313894e8ebd92432d97d4ac797d
Neobrinoke <neobrinoke@gmail.com>
2018-05-12 13:13:54 +0200

    remove phinx and create my own container

--- application/src/Framework/Container/Container.php
+++ application/src/Framework/Container/Container.php
@@ -2,22 +2,80 @@
 
 namespace App\Framework\Container;
 
-class Container extends \DI\Container
+use Exception;
+use ReflectionClass;
+use ReflectionException;
+
+class Container
 {
 	/** @var Container */
-	protected static $instance = null;
+	private static $instance = null;
+
+	/** @var array */
+	private $instances = [];
+
+	/** @var array */
+	private $resolvers = [];
 
 	/**
-	 * Retrieve instance of the current container
+	 * Get instance of this container
 	 *
 	 * @return Container
 	 */
-	public static function instance(): Container
+	public static function getInstance(): Container
 	{
 		if (is_null(static::$instance)) {
-			static::$instance = new static;
+			static::$instance = new static();
 		}
 
 		return static::$instance;
+	}
+
+	/**
+	 * Insert resolver
+	 *
+	 * @param $key
+	 * @param $resolver
+	 */
+	public function set($key, $resolver)
+	{
+		$this->resolvers[$key] = $resolver;
+	}
+
+	/**
+	 * @param $key
+	 * @return mixed
+	 * @throws ReflectionException
+	 * @throws Exception
+	 */
+	public function get($key)
+	{
+		if (!isset($this->instances[$key])) {
+			if (isset($this->resolvers[$key])) {
+				$this->instances[$key] = $this->resolvers[$key]();
+			} else {
+				$reflectedClass = new ReflectionClass($key);
+				if ($reflectedClass->isInstantiable()) {
+					$constructor = $reflectedClass->getConstructor();
+					if ($constructor) {
+						$parameters = [];
+						foreach ($reflectedClass->getConstructor()->getParameters() as $parameter) {
+							if ($parameter->getClass()) {
+								$parameters[] = $this->get($parameter->getClass()->getName());
+							} else {
+								$parameters[] = $parameter->getDefaultValue();
+							}
+						}
+						$this->instances[$key] = $reflectedClass->newInstanceArgs($parameters);
+					} else {
+						$this->instances[$key] = $reflectedClass->newInstance();
+					}
+				} else {
+					throw new Exception(sprintf("%s is not an instantiable Class", $key));
+				}
+			}
+		}
+
+		return $this->instances[$key];
 	}
 }
